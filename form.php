027b5d11a2fbe90a28ed61ad1dfe8bc7bb260807
Aashi Mundra <46136404+aashi3011@users.noreply.github.com>
2020-04-23 11:27:32 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+
+$a= $_POST ['a'];
+$b= $_POST ['b'];
+$c= $_POST ['c'];
+$d= $_POST ['d'];
+$e= $_POST ['e'];
+$f= $_POST ['f'];
+$g= $_POST ['g'];
+$h= $_POST ['h'];
+$i= $_POST ['i'];
+$j= $_POST ['j'];
+echo $a;
+
+?>
